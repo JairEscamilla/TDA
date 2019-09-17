aaeeb5134bad5b38f0620aff9da5dbe5177c58bd
Jair Escamilla <hernandez_yo14@outlook.es>
2019-09-17 10:33:09 -0500

    Generando nodos

--- ej3/grafos.c
+++ ej3/grafos.c
@@ -24,12 +24,13 @@ typedef struct defLD{
 }LD;
 
 // Prototipos de funciones
-void anadirNodo(Nodo** Inicio, int conexiones, char* etiqueta);
+void anadirNodo(LD** Init, int conexiones, char* etiqueta);
 Nodo** generarArray(int conexiones);
+void agregaraListaD(LD** Init, Nodo* tempo);
 
 int main(){
   Nodo* Inicio = NULL;
-  LD* Init = NULL;
+  LD* Init = NULL, *temp;
   int conexiones;
   int opc = 0;
   char etiqueta = 'a';
@@ -39,20 +40,43 @@ int main(){
     printf("1.- Aniadir nodo.\n");
     printf("Ingresar opcion-> ");
     scanf("%d", &opc);
-    anadirNodo(&Inicio, conexiones, &etiqueta);
+    anadirNodo(&Init, conexiones, &etiqueta);
   } while(opc != 0);
+  temp = Init;
+  while (temp != NULL) {
+    printf("%c ", temp->nd->etiqueta);
+    temp = temp->sig;
+  }
+  printf("\n");
   return 0;
 }
 
-void anadirNodo(Nodo** Inicio, int conexiones, char* etiqueta){
+void anadirNodo(LD** Init, int conexiones, char* etiqueta){
   Nodo* temp = (Nodo*)malloc(sizeof(Nodo));
   temp->etiqueta = *etiqueta;
   temp->conexiones = generarArray(3);
   for(int i = 0; i < conexiones; i++){
     temp->conexiones[i] = NULL;
   }
+  *etiqueta = *etiqueta + 1;
+  agregaraListaD(Init, temp);
 }
 
 Nodo** generarArray(int conexiones){
   return (Nodo**)malloc(conexiones*sizeof(Nodo*));
+}
+
+void agregaraListaD(LD** Init, Nodo* tempo){
+  LD* temp = (LD*)malloc(sizeof(LD)), *temp2;
+  temp->nd = tempo;
+  temp->sig = NULL;
+  if(*Init == NULL)
+    (*Init) = temp;
+  else{
+    temp2 = *Init;
+    while (temp2->sig != NULL) {
+      temp2 = temp2->sig;
+    }
+    temp2->sig = temp;
+  }
 }
