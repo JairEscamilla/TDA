633f19abeb43b461efb18185c9a0bc85735f2967
Jair Escamilla <hernandez_yo14@outlook.es>
2019-09-17 10:22:14 -0500

    Creando nodo

--- ej3/grafos.c
+++ ej3/grafos.c
@@ -7,3 +7,52 @@
  * -> Implementar las funciones de : anadir_nodo, anadir_vertice, desplegar_grafo, buscar_dato.
  * -> Cada nodo será etiquetado por su orden de insercion.
  */
+
+#include <stdio.h> // Incluyendo las librerias
+#include <stdlib.h>
+
+// Definiendo la estructura del nodo
+typedef struct defNodo{
+  char etiqueta;
+  int cantidadConexiones;
+  struct defNodo** conexiones;
+}Nodo;
+
+typedef struct defLD{
+  Nodo* nd;
+  struct defLD* sig;
+}LD;
+
+// Prototipos de funciones
+void anadirNodo(Nodo** Inicio, int conexiones, char* etiqueta);
+Nodo** generarArray(int conexiones);
+
+int main(){
+  Nodo* Inicio = NULL;
+  LD* Init = NULL;
+  int conexiones;
+  int opc = 0;
+  char etiqueta = 'a';
+  printf("Ingresar la cantidad de conexiones-> ");
+  scanf("%d", &conexiones);
+  do {
+    printf("1.- Aniadir nodo.\n");
+    printf("Ingresar opcion-> ");
+    scanf("%d", &opc);
+    anadirNodo(&Inicio, conexiones, &etiqueta);
+  } while(opc != 0);
+  return 0;
+}
+
+void anadirNodo(Nodo** Inicio, int conexiones, char* etiqueta){
+  Nodo* temp = (Nodo*)malloc(sizeof(Nodo));
+  temp->etiqueta = *etiqueta;
+  temp->conexiones = generarArray(3);
+  for(int i = 0; i < conexiones; i++){
+    temp->conexiones[i] = NULL;
+  }
+}
+
+Nodo** generarArray(int conexiones){
+  return (Nodo**)malloc(conexiones*sizeof(Nodo*));
+}
