fa5ea18c060f589d07ede853dbb5dd803939f153
Jair Escamilla <hernandez_yo14@outlook.es>
2019-09-17 09:53:58 -0500

    Implementando un generador de nodos

---
+++ ej3/grafos.c
@@ -0,0 +1,9 @@
+/*
+ * @author:  Allan Jair Escamilla Hernández
+ * @date:    17/septiembre/2019
+ * @file:    grafos.c
+ * @brief:  Implementacion de un generador de grafos.
+ * -> Con una estructura de datos en C generar un tipo de datos "nodo" que contenga n conexiones definidas por el usuario.
+ * -> Implementar las funciones de : anadir_nodo, anadir_vertice, desplegar_grafo, buscar_dato.
+ * -> Cada nodo será etiquetado por su orden de insercion.
+ */
