2bf489f202b5781bc2b164dd3eec01c4a15144a6
Jair Escamilla <hernandez_yo14@outlook.es>
2019-10-12 17:00:27 -0500

    Comenzando con Dijkstra

---
+++ grafos/dijkstra.h
@@ -0,0 +1,9 @@
+/*
+ * @author:  Allan Jair Escamilla Hernández
+ * @date:    12/octubre/2019
+ * @file:    dijkstra.c
+ * @brief:  Implementacion del algortmo Dijkstra a un grafo.
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
