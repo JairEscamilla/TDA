20c3291565d40bbbfcdc9831cb1aceaf792ae14b
Jair Escamilla <hernandez_yo14@outlook.es>
2019-10-18 20:03:50 -0500

    Mejorando las neuronas con valores predeterminados

--- Neuronas/neuronasValoresPuestos/neuronas.c
+++ Neuronas/neuronasValoresPuestos/neuronas.c
@@ -8,6 +8,7 @@ typedef struct Neuronas{
 
 double sumatoria(int x[], Neurona n);
 int activacion(double sum);
+void iniciarNeuronas(Neurona* and, Neurona* or, Neurona* not);
 
 int main(){
     Neurona and, or, not;
@@ -16,40 +17,42 @@ int main(){
     and.w = (double*)malloc(sizeof(double)*2);
     or.w = (double*)malloc(sizeof(double)*2);
     not.w = (double*)malloc(sizeof(double)*2);
-    and.w[0] = 0.7;
-    and.w[0] = 0.3;
-    and.bias = -1;
-    or.w[0] = 1.61;
-    or.w[0] = 0.41;
-    or.bias = 2.02;
-    not.w[0] = -1.2;
-    not.w[0] = 0;
-    not.bias = 1;
+    iniciarNeuronas(&and, &or, &not);
     do{
-        printf("1.- and\n2.-or\n3.-not\n4.-salir");
+        system("clear");
+        printf("1.-and\n2.-or\n3.-not\n4.-salir\nElegir una opcion: ");
         scanf("%d", &op);
         switch (op){
         case 1:
-            printf("Ingresar datos: ");
-            for(int i = 0; i < 2; i++)
+            for(int i = 0; i < 2; i++){
+                printf("Ingresar entrada numero %d-> ", i);
                 scanf("%d", &(x[i]));
+            }
             printf("El resultado es %d\n", activacion(sumatoria(x, and)));
             break;
         case 2:
-            printf("Ingresar datos: ");
-            for(int i = 0; i < 2; i++)
+            for(int i = 0; i < 2; i++){
+                printf("Ingresar entrada numero %d-> ", i);
                 scanf("%d", &(x[i]));
+            }
             printf("El resultado es %d\n", activacion(sumatoria(x, or)));
             break;
         case 3:
-            printf("Ingresar datos: ");
-            for(int i = 0; i < 2; i++)
-                scanf("%d", &(x[i]));
+            printf("Ingresar entrada: ");
+            scanf("%d", &(x[0]));
+            x[1] = 0;
             printf("El resultado es %d\n", activacion(sumatoria(x, not)));
             break;
+        case 4: 
+            printf("Saliendo\n");
+            break;
         default:
+            printf("Ingresa una opcion valida!\n");
             break;
         }
+        printf("Presionar enter para continuar...\n");
+        __fpurge(stdin);
+        getchar();
     } while (op != 4);
     
 }   
@@ -60,6 +63,19 @@ double sumatoria(int x[], Neurona n){
         sum+= x[i]*n.w[i];
     return sum + n.bias; 
 }
+
+void iniciarNeuronas(Neurona* and, Neurona* or, Neurona* not){
+    and->w[0] = 1;
+    and->w[1] = 1;
+    and->bias = -1;
+    or->w[0] = 1;
+    or->w[1] = 1;
+    or->bias = 0;
+    not->w[0] = -1;
+    not->w[1] = 0;
+    not->bias = 0.75;
+}
+
 int activacion(double sum){
-    return sum >= 0;
+    return sum >= 0.5;
 }
