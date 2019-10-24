66028c8379182672ca96de713d461904a96f2fd2
Jair Escamilla <hernandez_yo14@outlook.es>
2019-10-24 10:59:38 -0500

    INtentando

--- Neuronas/neuronasValoresPuestos/neuronas.c
+++ Neuronas/neuronasValoresPuestos/neuronas.c
@@ -8,27 +8,35 @@
 // INCLUYENDO BIBLIOTECAS
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 // DEFINIENDO LA ESTRUCTURA QUE TENDRA UNA NEURONA
 typedef struct Neuronas{
     double* w;
     double bias;
+    double* error;
 }Neurona;
 
 // PROTOTIPOS DE LAS FUNCIONES
 double sumatoria(int x[], Neurona n);
 int activacion(double sum);
 void iniciarNeuronas(Neurona* and, Neurona* or, Neurona* not);
+double randfrom(double min, double max);
+void entrenarNeurona(Neurona* neuron, int inputs[4][2], int outputs[]);
 
 // FUNCION PRINCIPAL
 int main(){
     Neurona and, or, not; // Declarando las neuronas
     int op;
+    int inputsAnd[4][2] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}}; 
+    int outputsAnd[4] = {0, 0, 0, 1};
     int *x = (int*)malloc(sizeof(int)*2);
     // ASIGNANDO MEMORIA A LOS ARREGLOS QUE CONTENDRAN LOS PESOS DE LAS ENTRADAS
     and.w = (double*)malloc(sizeof(double)*2);
+    and.error = (double*)malloc(sizeof(double)*2);
     or.w = (double*)malloc(sizeof(double)*2);
     not.w = (double*)malloc(sizeof(double)*2);
     iniciarNeuronas(&and, &or, &not); // Inicializando los valores de las neuronas
+    entrenarNeurona(&and, inputsAnd, outputsAnd);
     do{
         system("clear");
         printf("1.-and\n2.-or\n3.-not\n4.-salir\nElegir una opcion: ");
@@ -91,9 +99,9 @@ double sumatoria(int x[], Neurona n){
    * @param Neurona* not recibe una neurona para asignar pesos.
 */
 void iniciarNeuronas(Neurona* and, Neurona* or, Neurona* not){
-    and->w[0] = 1;
-    and->w[1] = 1;
-    and->bias = -1;
+    and->w[0] = randfrom(0, 1);
+    and->w[1] = randfrom(0, 1);
+    and->bias = randfrom(0, 1); 
     or->w[0] = 1;
     or->w[1] = 1;
     or->bias = 0;
@@ -108,3 +116,32 @@ void iniciarNeuronas(Neurona* and, Neurona* or, Neurona* not){
 int activacion(double sum){
     return sum >= 0.5;
 }
+double randfrom(double min, double max) {
+    double range = (max - min); 
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+}
+
+void entrenarNeurona(Neurona* neuron, int inputs[4][2], int outputs[]){
+    FILE* Arch = fopen("neuronas.dat", "wt");
+    int epochs = 10000;
+    int counter = 0;
+    double salida;
+    double sum = 0, lr = 0.0001;
+    for(int i = 0; i < epochs; i++){
+        counter = 0;
+        while (counter < 4){
+            sum = 0;
+            sum = sumatoria(inputs[counter], *neuron);
+            //salida = sum >= 0;
+            for(int j = 0; j < 2; j++)
+                neuron->w[counter] = neuron->w[counter] + lr*(sum - outputs[counter])*inputs[counter][j];
+
+            neuron->bias = neuron->bias + lr*(sum - outputs[counter]);
+            counter++;
+        }
+        fprintf(Arch, "%f, %f, %f, %f\n", neuron->w[0], neuron->w[1], neuron->bias, sum - outputs[counter]);
+    }
+    fclose(Arch);
+}
+
